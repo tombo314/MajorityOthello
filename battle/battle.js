f0922267a6ab8a2d3feb8d166e50334571d2c564
unknown <sy021026@st.nuis.jp>
2022-10-07 23:53:53 +0900

    josisu

--- battle/battle.js
+++ battle/battle.js
@@ -1,26 +1,24 @@
 const socket = io();
 
-const nodes = document.getElementById("nodes");
+const nodesAlly = document.getElementById("nodes-ally");
 socket.emit("battle-start", {value: ""});
 socket.on("battle-start", (data)=>{
     let users = data.value;
     initX = 160;
     initY = 240;
     radius = 20;
-    let elem = document.createElement("canvas");
-    nodes.appendChild(elem);
+    elem = document.createElement("canvas");
     elem.setAttribute("id", "own");
-    elem.setAttribute("class", "node");
     elem.setAttribute("width", 300);
     elem.setAttribute("height", 300);
+    nodesAlly.appendChild(elem);
     own = document.getElementById("own");
-    let context = elem.getContext("2d");
+    context = elem.getContext("2d");
     context.beginPath();
     context.arc(initX, initY, radius, 0*Math.PI/180, 360*Math.PI/180, false);
     context.fillStyle = "rgba(255, 172, 32, 1)";
     context.fill();
     context.stroke();
-    updateStarted = true;
 });
 
 let initX;
@@ -33,7 +31,8 @@ let wDown;
 let aDown;
 let sDown;
 let dDown;
-let updateStarted;
+let elem;
+let context;
 onkeydown=(e)=>{
     if (e.key=="w"){
         wDown = true;
