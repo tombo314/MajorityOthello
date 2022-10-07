d413df7004dbf8a07c5ebfb0bcb578443ae7a1e6
unknown <sy021026@st.nuis.jp>
2022-10-07 10:05:23 +0900

    josisu

--- battle/battle.js
+++ battle/battle.js
@@ -4,16 +4,19 @@ const nodes = document.getElementById("nodes");
 socket.emit("battle-start", {value: ""});
 socket.on("battle-start", (data)=>{
     let users = data.value;
+    initX = 100;
+    initY = 200;
+    radius = 20;
     let elem = document.createElement("canvas");
     nodes.appendChild(elem);
     elem.setAttribute("id", "own");
     elem.setAttribute("class", "node");
-    const own = document.getElementById("own");
-    // initX = own.getBoundingClientRect().x;
-    // initY = own.getBoundingClientRect().y;
+    elem.setAttribute("width", innerWidth);
+    elem.setAttribute("height", innerHeight);
+    own = document.getElementById("own");
     let context = elem.getContext("2d");
     context.beginPath();
-    context.arc(200, 40, 20, 0*Math.PI/180, 360*Math.PI/180, false);
+    context.arc(initX, initY, radius, 0*Math.PI/180, 360*Math.PI/180, false);
     context.fillStyle = "rgba(255, 172, 32, 1)";
     context.fill();
     context.stroke();
@@ -21,29 +24,31 @@ socket.on("battle-start", (data)=>{
 
 let initX;
 let initY;
+let radius;
 let x = 0;
 let y = 0;
+let own;
 onkeydown=(e)=>{
     if (e.key=="w"){
-        if (initY+y-10>=-10){
+        if (initY+y-radius*3/2>=0){
             y -= 10;
         }
     }
     if (e.key=="a"){
-        if (x-10>=-10 || true){
+        if (initX+x-radius*3/2>=0){
             x -= 10;
         }
     }
     if (e.key=="s"){
-        if (initY+y+10<=innerHeight){
+        if (initY+y-radius<=innerHeight){
             y += 10;
         }
     }
     if (e.key=="d"){
-        if (initX+x+10<=innerWidth || true){
+        if (initX+x-radius<=innerWidth){
             x += 10;
         }
     }
     own.style.transform = `translate(${x}px, ${y}px)`;
-    console.log(own.getBoundingClientRect().x);
+    // console.log(own.getBoundingClientRect().x+firstX, own.getBoundingClientRect().y+firstY);
 }
