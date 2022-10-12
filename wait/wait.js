74fd2abebbdfcb7561da4f55f517c342df074ef8
unknown <sy021026@st.nuis.jp>
2022-10-12 10:51:28 +0900

    josisu

--- wait/wait.js
+++ wait/wait.js
@@ -2,7 +2,7 @@ const socket = io();
 
 socket.emit("waiting-started", {value: ""});
 socket.on("waiting-started", (data)=>{
-    const waitForSeconds = 3;
+    const waitForSeconds = 20;
     const waitFor = waitForSeconds*1000;
     let now;
     const func=()=>{
