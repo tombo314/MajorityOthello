ae9a2987aed97161913abc8cb1e83ebd43c85e5e
unknown <sy021026@st.nuis.jp>
2022-10-18 15:11:19 +0900

    majority-othello

--- main/main.js
+++ main/main.js
@@ -5,6 +5,7 @@ const btnMakeRoom = document.getElementById("make-room");
 const btnEnterRoom = document.getElementById("enter-room");
 const roomMakeWindow = document.getElementById("room-make-window");
 const roomName = document.getElementById("room-name");
+const roomPassword = document.getElementById("room-password");
 
 // ユーザー名入力
 let username;
@@ -27,30 +28,50 @@ socket.on("need-users", (data)=>{
     }
 });
 
+// 周りの暗いところをクリックしてキャンセル
 blackSheet.onclick=()=>{
     blackSheet.style.visibility = "hidden";
     roomMakeWindow.style.visibility = "hidden";
     roomName.value = "";
 }
 
+// 「部屋を作る」を押してフォームを表示
 btnMakeRoom.onclick=()=>{
     blackSheet.style.visibility = "visible";
     roomMakeWindow.style.visibility = "visible";
     roomName.value = "部屋名を入力してください。";
+    roomPassword.value = "パスワードを入力してください。";
     roomName.style.color = "#2227";
+    roomPassword.style.color = "#2227";
     roomName.focus();
 }
 
+// 文字が入力されたら「部屋名を～」をクリア
 roomName.onkeydown=(e)=>{
     if (roomName.value=="部屋名を入力してください。"){
         roomName.value = "";
-        cursor00 = false;
         roomName.style.color = "#222";
     }
 }
-onkeyup=()=>{
+// value が空のとき「部屋名を～」を表示
+roomName.onkeyup=()=>{
     if (roomName.value==""){
         roomName.value = "部屋名を入力してください。";
         roomName.style.color = "#2227";
+    }
+}
+
+// 文字が入力されたら「パスワードを～」をクリア
+roomPassword.onkeydown=(e)=>{
+    if (roomPassword.value=="パスワードを入力してください。"){
+        roomPassword.value = "";
+        roomPassword.style.color = "#222";
+    }
+}
+// value が空のとき「パスワードを～」を表示
+roomPassword.onkeyup=()=>{
+    if (roomPassword.value==""){
+        roomPassword.value = "パスワードを入力してください。";
+        roomPassword.style.color = "#2227";
     }
 }
